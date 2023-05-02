0a0e16eafb21ed1b943d9c80b028ef2ddf82dbae
jp durigan <jpdurigan@gmail.com>
2023-05-02 16:33:33 -0300

    fix changescenebutton

--- src/ui/buttons/ChangeSceneButton.gd
+++ src/ui/buttons/ChangeSceneButton.gd
@@ -1,8 +1,7 @@
 tool
 extends Button
 
-export var change_to: PackedScene = null setget _set_change_to, _get_change_to
-export var scene_path: String
+var scene_path: String
 
 
 func _ready():
@@ -13,14 +12,41 @@ func _on_pressed():
 	SceneChanger.change_to(scene_path)
 
 
-func _set_change_to(scene: PackedScene):
-	if scene:
-		scene_path = scene.resource_path
-		property_list_changed_notify()
+func _get(property):
+	match property:
+		"change_to":
+			if scene_path and ResourceLoader.exists(scene_path):
+				var scene = ResourceLoader.load(scene_path) as PackedScene
+				return scene
+		"_scene_path":
+			return scene_path
 
-func _get_change_to() -> PackedScene:
-	if scene_path and ResourceLoader.exists(scene_path):
-		return ResourceLoader.load(scene_path) as PackedScene
-	else:
-		scene_path = ""
-		return null
+func _set(property, value):
+	match property:
+		"change_to":
+			var scene: PackedScene = value as PackedScene
+			if scene:
+				scene_path = scene.resource_path
+		"_scene_path":
+			scene_path = value
+
+func _get_property_list() -> Array:
+	var properties := []
+	
+	properties.append_array([
+		{
+			"name": "change_to",
+			"type": TYPE_OBJECT,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "PackedScene",
+			"usage": PROPERTY_USAGE_EDITOR,
+		},
+		{
+			"name": "_scene_path",
+			"type": TYPE_STRING,
+			"hint": PROPERTY_HINT_GLOBAL_FILE,
+			"usage": PROPERTY_USAGE_NOEDITOR,
+		}
+	])
+	
+	return properties
